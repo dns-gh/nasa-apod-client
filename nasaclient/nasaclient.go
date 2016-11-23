dc2b8bff5bb6b7045eab007c7493ff8fe1715b71
Dimitri <dns@masagroup.net>
2016-11-23 12:40:16 +0100

    check service version and displays warning if different and add archive url output data when fetching the apod

--- nasaclient/nasaclient.go
+++ nasaclient/nasaclient.go
@@ -13,9 +13,15 @@ import (
 )
 
 const (
-	nasaApodAPIGet    = "https://api.nasa.gov/planetary/apod?api_key="
-	nasaAPIDefaultKey = "DEMO_KEY"
-	nasaTimeFormat    = "2006-01-02"
+	nasaApodAPIGet = "https://api.nasa.gov/planetary/apod?api_key="
+
+	// The archive page of a picture of a particular day can be retrieve via the following info.
+	nasaApodArchivePage          = "https://apod.nasa.gov/apod/ap"
+	nasaApodArchivePageExtension = ".html"
+
+	nasaApodServiceVersion = "v1"
+	nasaAPIDefaultKey      = "DEMO_KEY"
+	nasaTimeFormat         = "2006-01-02"
 )
 
 // NasaApodClient represents the web Client.
@@ -49,10 +55,14 @@ type Apod struct {
 	URL            string `json:"url"`
 }
 
+func makeArchiveDate(date string) string {
+	return nasaApodArchivePage + strings.Replace(date[2:], "-", "", 2) + nasaApodArchivePageExtension
+}
+
 // FetchAPOD fetches metadata information of the image of the given 'date'
 // in 'hd' format potentially.
 // The 'date' must be in the following format: 'YYYY-MM-DD'
-func (n *NasaApodClient) FetchAPOD(date string, hd bool) (*Apod, error) {
+func (n *NasaApodClient) FetchAPOD(date string, hd bool) (*Apod, string, error) {
 	url := nasaApodAPIGet +
 		n.apiKey
 	if len(date) != 0 {
@@ -63,24 +73,27 @@ func (n *NasaApodClient) FetchAPOD(date string, hd bool) (*Apod, error) {
 	}
 	resp, err := http.Get(url)
 	if err != nil {
-		return nil, err
+		return nil, "", err
 	}
 	defer resp.Body.Close()
 	bytes, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return nil, err
+		return nil, "", err
 	}
 	if strings.Contains(string(bytes), "OVER_RATE_LIMIT") {
-		return nil, fmt.Errorf("http get rate limit reached, wait or use a proper key instead of the default one")
+		return nil, "", fmt.Errorf("http get rate limit reached, wait or use a proper key instead of the default one")
 	}
 	apod := &Apod{}
 	json.Unmarshal(bytes, apod)
-	return apod, nil
+	if apod.ServiceVersion != nasaApodServiceVersion {
+		log.Printf("[WARNING] remote service version %s != %s\n", apod.ServiceVersion, nasaApodServiceVersion)
+	}
+	return apod, makeArchiveDate(apod.Date), nil
 }
 
 // FetchTodayAPOD fetches metadata information of today's Apod image
 // in 'hd' format potentially.
-func (n *NasaApodClient) FetchTodayAPOD(hd bool) (*Apod, error) {
+func (n *NasaApodClient) FetchTodayAPOD(hd bool) (*Apod, string, error) {
 	return n.FetchAPOD("", hd)
 }
 
@@ -102,14 +115,14 @@ func loadImage(uri string) (string, error) {
 }
 
 // FetchHD fetches the apod today's image and description.
-func (n *NasaApodClient) FetchHD() (string, string, error) {
-	apod, err := n.FetchTodayAPOD(true)
+func (n *NasaApodClient) FetchHD() (string, string, string, error) {
+	apod, archiveURL, err := n.FetchTodayAPOD(true)
 	if err != nil {
-		return "", "", err
+		return "", "", "", err
 	}
 	img, err := loadImage(apod.URL)
 	if err != nil {
-		return "", "", err
+		return "", "", "", err
 	}
-	return apod.Explanation, img, nil
+	return apod.Explanation, img, archiveURL, nil
 }
